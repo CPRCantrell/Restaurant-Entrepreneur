52c8a01e2c5321f5ecd87e8154c30b0f5b554707
Chris Figueroa <Chris.FigueroaPerez@gmail.com>
2023-03-01 01:48:05 -0500

    📦 NEW: now have a singleton patter for logger

--- logger.py
+++ logger.py
@@ -3,5 +3,12 @@ class Logger:
         self.transaction_count = 0
         self.daily_sales = 0
 
-    def log_transaction(self, order, sale):
-        pass
+    def log_transaction(self, order, store_number):
+        self.transaction_count += 1
+        self.daily_sales += order.price
+
+        file = open('log.txt', 'a')
+        file.write(f'TRAN #{self.transaction_count}: \n\tOrdered: {order.dish_name}\n\tFrom: {store_number}\n\tPrice: ${order.price}\t\t\tToday\'s total: ${self.daily_sales}\n')
+        file.close()
+
+logger = Logger()
