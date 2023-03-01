4977eefcede31db042c6651a958e048971552a57
Chris Figueroa <Chris.FigueroaPerez@gmail.com>
2023-03-01 02:21:30 -0500

    📦 NEW: Verifying user input in franchise

--- franchise.py
+++ franchise.py
@@ -1,3 +1,5 @@
+import os
+import sys
 from logger import logger
 from order_factory import OrderFactory as orfact
 
@@ -6,6 +8,41 @@ class Franchise:
         self. location_number = location_number
 
     def place_order(self):
-        user_input = input('Order options:\n\t1) Pizza\n\t2) Pasta\n\t3) Salad\nWhat would you like to order: ')
+        user_input = self.__collect_valid_input__()
         order = orfact.create_order(user_input)
         logger.log_transaction(order,self.location_number)
+        Franchise.__clear_screen__()
+
+    def __collect_valid_input__(self):
+        options = ['pizza','pasta','salad']
+
+        print(f'Your ordering options (store: {self.location_number}):')
+        for option in range(len(options)):
+            print(f'{option + 1}) {options[option].title()}')
+
+        while True:
+            user_input = input('Which option would you like: ').lower()
+            Franchise.__clear_line__()
+
+            if user_input in options:
+                return user_input
+
+            try:
+                user_input = int(user_input)
+                return options[user_input-1]
+            except: pass
+
+            print('INVALID ENTRY: enter name or number : ',end='')
+
+    def __clear_screen__():
+        # for windows
+        if os.name == 'nt':
+            os.system('cls')
+        # for mac and linux(here, os.name is 'posix')
+        else:
+            os.system('clear')
+
+    def __clear_line__(lines = 1):
+        for line in range(lines):
+            sys.stdout.write("\033[F")
+            sys.stdout.write("\033[K")
