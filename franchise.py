d10cfe907599e5f30a7eae479f5aec5cd6614d4b
Chris Figueroa <Chris.FigueroaPerez@gmail.com>
2023-03-01 01:55:00 -0500

    📦 NEW: Franchise can now take orders and track them with logger

--- franchise.py
+++ franchise.py
@@ -1,6 +1,11 @@
+from logger import logger
+from order_factory import OrderFactory as orfact
+
 class Franchise:
     def __init__(self, location_number) -> None:
         self. location_number = location_number
 
     def place_order(self):
-        pass
+        user_input = input('Order options:\n\t1) Pizza\n\t2) Pasta\n\t3) Salad\nWhat would you like to order: ')
+        order = orfact.create_order(user_input)
+        logger.log_transaction(order,self.location_number)
